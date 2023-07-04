ecb492f788e2b1ab09046cf0ed9ce1b6e6e61bc0
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-04 15:58:12 +0300

    add: section-hero add list items

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
 const languageBtn = document.getElementById("headerBtn");
+const listItems = document.getElementsByClassName("section-hero__list-item");
+const sectionHero = document.getElementById("sectionHero");
 
-languageBtn.addEventListener("click");
+for (let item of listItems) {
+  item.addEventListener("click", function () {
+    for (let item2 of listItems) {
+      item2.classList.remove("active-item");
+    }
+    item.classList.add("active-item");
+    const topic = item.getAttribute("data-topic");
+    sectionHero.style.backgroundImage = `linear-gradient(
+        rgba(0, 0, 0, 0.4),
+        rgba(0, 0, 0, 0.4)
+      ),
+      url("/assets/${topic}.webp")`;
+  });
+}
