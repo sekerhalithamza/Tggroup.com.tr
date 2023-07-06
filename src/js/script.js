f392b8d2d648e6f9d07e6e044ddb3e829a270711
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-06 17:38:54 +0300

    add: section references and other stuff

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,9 @@
 const languageBtn = document.getElementById("headerBtn");
 const listItems = document.getElementsByClassName("section-hero__list-item");
 const sectionHero = document.getElementById("sectionHero");
+const textElements = document.getElementsByClassName("text");
+const gallery = document.getElementById("gallery");
+const chevronBtns = document.getElementsByClassName("section-references--chevron");
 
 for (let item of listItems) {
   item.addEventListener("click", function () {
@@ -16,5 +19,31 @@ for (let item of listItems) {
         rgba(0, 0, 0, 0.4)
       ),
       url("/assets/${topic}.webp")`;
+  });
+}
+
+languageBtn.addEventListener("click", function () {
+  for (let textItem of textElements) {
+    let oldText = textItem.innerText;
+    textItem.innerText = textItem.getAttribute("data-language");
+    textItem.setAttribute("data-language", oldText);
+  }
+});
+
+var galleryPosition = 0;
+
+// Min vw: translatex(300vw)
+// Max vw: translateX(300vw)
+for (let btn of chevronBtns) {
+  btn.addEventListener("click", function () {
+    let direction = btn.getAttribute("data-direction");
+    if (direction === "right")
+      if (galleryPosition <= -300) {
+        galleryPosition *= -1;
+      } else galleryPosition -= 20;
+    else if (galleryPosition >= 300) {
+      galleryPosition *= -1;
+    } else galleryPosition += 20;
+    gallery.style.transform = `translateX(-50%) translateY(-50%) translateX(${galleryPosition}vw)`;
   });
 }
